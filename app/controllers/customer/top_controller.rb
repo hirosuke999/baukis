241dade83ba488b0af2810a75c404f964187c878
hirosuke999 <jeuxleaveus@gmail.com>
2017-09-10 05:17:35 +0900

    例外処理を実装 404 Not Found

--- app/controllers/customer/top_controller.rb
+++ app/controllers/customer/top_controller.rb
@@ -1,5 +1,5 @@
 class Customer::TopController < ApplicationController
   def index
-    raise Forbidden
+    raise ActiveRecord::RecordNotFound
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -56,13 +56,22 @@ Rails.application.routes.draw do
 
   namespace :staff do
     root 'top#index'
+    get 'login' => 'session#new', as: :login
+    post 'session' => 'session#create', as: :session
+    delete 'session' => 'session#destroy'
   end
 
   namespace :admin do
     root 'top#index'
+    get 'login' => 'session#new', as: :login
+    post 'session' => 'session#create', as: :session
+    delete 'session' => 'session#destroy'
   end
 
   namespace :customer do
     root 'top#index'
   end
+
+  root 'errors#routing_error'
+  get '*anything' => 'errors#routing_error'
 end
